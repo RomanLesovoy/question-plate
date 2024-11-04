f1673d48ea3b2cbf4026711074afc4afc9d171af
RomanLesovoy <romadesire97@gmail.com>
2024-11-04 16:41:00 +0200

    init ionic

---
+++ question-plate-view/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'question-plate-view',
+  webDir: 'www'
+};
+
+export default config;
