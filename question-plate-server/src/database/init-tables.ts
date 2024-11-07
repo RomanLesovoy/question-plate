40da93050f8e5788435dc30eefc685a20ad5ea9d
RomanLesovoy <romadesire97@gmail.com>
2024-11-07 18:15:22 +0200

    fix: migrations sequence

--- question-plate-server/src/database/init-tables.ts
+++ question-plate-server/src/database/init-tables.ts
@@ -1,6 +1,6 @@
 import { Knex } from 'knex';
-import { up as createAnsweredQuestionsTable } from './migrations/create_answered_questions';
-import { up as createUsersTable } from './migrations/create_users';
+import { up as createAnsweredQuestionsTable } from './migrations/20240301000002_create_answered_questions';
+import { up as createUsersTable } from './migrations/20240301000001_create_users';
 
 export async function initTables(knex: Knex) {
   // Check if tables exist
