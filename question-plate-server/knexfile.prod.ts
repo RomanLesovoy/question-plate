d82aab5c4a4018b2d473a155c99db40bea3cc55e
RomanLesovoy <romadesire97@gmail.com>
2024-11-07 14:14:05 +0200

    build fixes #2

---
+++ question-plate-server/knexfile.prod.ts
@@ -0,0 +1,21 @@
+import { config } from 'dotenv';
+config();
+
+module.exports = {
+  client: 'postgresql',
+  connection: {
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT || '5432'),
+    database: process.env.DB_NAME,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+  },
+  pool: {
+    min: 2,
+    max: 10,
+  },
+  migrations: {
+    directory: './dist/src/database/migrations',
+    tableName: 'knex_migrations',
+  },
+};
